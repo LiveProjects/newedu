037066002563c785a26b59c441716cb782ceb7aa
theliolife <theliolife@theliolifedeMacBook-Air.local>
2015-10-23 16:36:55 +0800

    add router

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+avalon.define({
+    $id: "test",
+    xxx: "引入内部模板"
+});
+
 $(document).ready(function(){
     var gl={
         search_submit:document.getElementById("search_submit"),
@@ -6,7 +11,7 @@ $(document).ready(function(){
         passWord:document.getElementById("passWord")
     };
     //搜索
-    gl.search_submit.onclick=function(){
+    $("#search_submit").on('click',function(){
         var that=$(this);
         $.ajax({
             url:'php/common/login.php',
@@ -20,7 +25,7 @@ $(document).ready(function(){
                 console.log(error);
             }
         });
-    };
+    });
     //简单登陆
     $(document).delegate("#login_submit",'click',function(){
         var data={};
