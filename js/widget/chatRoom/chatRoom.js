57d4f0817ae213dacf25b29bb702e178fc0f7064
theliolife <theliolife@theliolifedeMacBook-Air.local>
2015-10-28 20:15:08 +0800

    chat

--- js/widget/chatRoom/chatRoom.js
+++ js/widget/chatRoom/chatRoom.js
@@ -4,6 +4,13 @@
 avalon.ready(function(){
     avalon.define({
         $id:'chatRoom',
+        friendList:['Lio','Anna','Kobe','Mash'],
+        chooseFriend:function(){
+            $("#friend_list").fadeOut();
+        },
+        chatRoomBlur:function(){
+            $("#friend_list").fadeIn();
+        }
     });
 
     avalon.scan();
